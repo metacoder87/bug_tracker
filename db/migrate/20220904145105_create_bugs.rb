c9f5b62de5d7a50ce686fcb3470dbe5f5c7358c6
metacoder87 <gary.miller@gmx.com>
2022-09-04 11:45:16 -0400

    Adds bugs db

--- db/migrate/20220904145105_create_bugs.rb
+++ db/migrate/20220904145105_create_bugs.rb
@@ -1,6 +1,9 @@
 class CreateBugs < ActiveRecord::Migration[5.2]
   def change
     create_table :bugs do |t|
+      t.string :location
+      t.text :description
+      t.boolean :status
 
       t.timestamps
     end

b98591193011847d9f50d7a161485d19e724620f
metacoder87 <gary.miller@gmx.com>
2022-09-10 11:58:59 -0400

    Moves method to helper to DRY out the code

--- app/helpers/bugs_helper.rb
+++ app/helpers/bugs_helper.rb
@@ -1,2 +1,18 @@
 module BugsHelper
+    def solve_bug_button(bug)
+        link_to "#{bug.status? ? 'Reopen' : 'Resolve'} Bug", solve_bug_path,
+            method: :put,
+            remote: true,
+            class: solve_bug_button_class(bug),
+            id: 'solve_button'
+    end
+
+private
+    def solve_bug_button_class(bug)
+        if bug.status?
+            'btn btn-danger' # red button
+        else 
+            'btn btn-warning' # orange button
+        end
+    end
 end

b308d48ccfd4056e8af0a779c04767279a664c87
metacoder87 <gary.miller@gmx.com>
2022-09-17 14:52:29 -0400

    Adds new comment control logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+    def create
+        @comment = Comment.new(comment_params)
+        respond_to do |format|
+            if @comment.save
+                format.html { redirect_to bug_path(@comment.bug), notice: "Your comment was posted" }
+            else
+                format.html
+            end
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body, :bug_id)
+    end
+
 end

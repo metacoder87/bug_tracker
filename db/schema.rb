824251e837355bb9a557d53323f76d8b5bdbe8fb
metacoder87 <gary.miller@gmx.com>
2022-09-17 14:25:09 -0400

    Adds comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_04_145105) do
+ActiveRecord::Schema.define(version: 2022_09_12_064255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,13 @@ ActiveRecord::Schema.define(version: 2022_09_04_145105) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "bug_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bug_id"], name: "index_comments_on_bug_id"
+  end
+
+  add_foreign_key "comments", "bugs"
 end

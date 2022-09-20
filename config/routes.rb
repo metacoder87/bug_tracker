d923a40ec1dac66d532d33df222a5de11e103367
metacoder87 <gary.miller@gmx.com>
2022-09-20 13:07:36 -0400

    Adds resources for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  
+  
+  resource :comments
+  
   resources :bugs do
     member do
       put 'solve'
     end
   end
+
   root to: 'bugs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

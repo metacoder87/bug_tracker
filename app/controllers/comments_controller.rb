d80e2dfc78dcd120eb48db025bb12883086678b8
metacoder87 <gary.miller@gmx.com>
2022-09-23 12:53:49 -0400

    Adds JS formatting to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,12 @@ class CommentsController < ApplicationController
         respond_to do |format|
             if @comment.save
                 format.html { redirect_to bug_path(@comment.bug), notice: "Your comment was posted" }
+                format.json { render json: @comment, status: :created, location: @comment }
+                format.js
             else
                 format.html
+                format.json { render json: @comment.errors, status: :unprocessable_entity }
+                format.js
             end
         end
     end

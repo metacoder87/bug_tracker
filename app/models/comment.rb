bd336c34e509e3aa2a12f7a54cb1f89dadab07ba
metacoder87 <gary.miller@gmx.com>
2022-10-04 14:32:27 -0400

    Adds user association to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :bug
+  belongs_to :user
 
   validates :location, :description, presence: true
+  validates :user, presence: true
 
   scope :by_date, -> { order(created_at: :desc) }
   

abff05ce89baa2d9699d1feb22de5fabb3fb27ce
metacoder87 <gary.miller@gmx.com>
2022-10-04 14:31:16 -0400

    Updates the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_01_222645) do
+ActiveRecord::Schema.define(version: 2022_10_03_160537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2022_10_01_222645) do
     t.bigint "bug_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["bug_id"], name: "index_comments_on_bug_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema.define(version: 2022_10_01_222645) do
   end
 
   add_foreign_key "comments", "bugs"
+  add_foreign_key "comments", "users"
 end

21c26d958aefb9f67cab32129e94d0903c482a62
metacoder87 <gary.miller@gmx.com>
2022-11-14 04:36:47 -0500

    Updates the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_085511) do
+ActiveRecord::Schema.define(version: 2022_11_14_090101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 2022_11_14_085511) do
     t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["path"], name: "index_bugs_on_path", unique: true
+    t.index ["user_id"], name: "index_bugs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_11_14_085511) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "bugs", "users"
 end

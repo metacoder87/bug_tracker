5c8e442a33f84ba7ca5a96c1bea6084792cfd13d
metacoder87 <gary.miller@gmx.com>
2022-11-14 04:35:31 -0500

    Adds new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'bugs#index'
 
-  resources :bugs
+  resources :bugs do
+    collection do
+      get :active
+      get :archive
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
